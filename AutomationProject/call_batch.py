456438224df3b95fa2fd15b8d8725cb7477371d2
terry9026955 <terry9026955@yahoo.com.tw>
2022-07-11 12:38:57 +0800

    Read from batch file and write into log file

--- AutomationProject/call_batch.py
+++ AutomationProject/call_batch.py
@@ -1,46 +1,52 @@
 import subprocess
 from subprocess import Popen
+import time
+
+real_time = time.strftime("%Y%m%d_%H%M%S", time.localtime())
 
 def main():
-    p = subprocess.run("D:/Tinghao.Chen/Desktop/SMIGIT/AutomationProject/download_sourcecode.bat", stdout=subprocess.PIPE)   #D:\Tinghao.Chen\Desktop\SMIGIT\AutomationProject\download_sourcecode.bat
-    str_List = str(p.stdout.decode('cp950')).split('\r\n\r\n')
+    # Open and read batch file
+    p = subprocess.run("D:/Tinghao.Chen/Desktop/SMIGIT/AutomationProject/download_sourcecode.bat", stdout=subprocess.PIPE)   
+    str_List = str(p.stdout.decode('cp950')).split('\r\n\r\n')  # i.replace("\r\n", "")  #替換特定字串用
     
-    #print(str_List)
-
-    
-
-    #read batch file line by line (測試用)
-    for i in str_List:
-        #for j in i:
-        print(i)
-        print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-        
-    # i.replace("\r\n", "")  #替換特定字串用
-    
-    
+    # Write batch output into data.log (寫入)
     with open("data.log", "w") as dataWrite:
+        print("Writing str_List into data.log")
         for line in str_List:
             dataWrite.write(line)
             dataWrite.write("\n")
 
 
-    # 問題: 很奇怪的是執行完call_batch.py之後，這個python檔的code會自動刪減掉with open()這一大段(會回溯到我一開始寫的那個版本)
-    # 目前: 可以讀batch file，把裡面的資訊一行一行存取並寫入到data.log裡面
+    # Print SHA code of the data.log
+    with open("data.log", "r") as dataRead:
+        print("Reading SHA code")
+        dataSHA = dataRead.readlines()[19]  # Read the specific line (SHA code)
+    
+    # Write the SHA code into Debug_log file 
+    with open(real_time + "_Debug_log.log", "w") as dataWrite:
+        print("SHA code is written into Degub_log")
+        dataWrite.write(dataSHA)
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+    # 問題: 很奇怪的是執行完call_batch.py之後，這個python檔的code會自動回溯到我一上一個寫的版本
 
 
 
     
-    # Read + Write to log file in this block (測試用)
+    # read batch file line by line (測試用)
+    # for i in str_List:
+    #     print(i)
+    #     print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+
+# Read + Write to log file in this block (測試用)
     # with open("data.log", "r") as dataRead, open("data2.log", "w") as dataWrite:
     #     for line in dataRead:
     #         print(line)
     #         dataWrite.write(line)
     #     dataWrite.write("Writing file successful")
-        
-            
-
-    
-
-
-if __name__ == "__main__":
-    main()
